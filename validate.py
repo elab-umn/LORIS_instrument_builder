913c75b6be35a639259d26040dee66a0cfbd29ee
Barry Tikalsky <tikal004@umn.edu>
2023-04-12 13:38:18 -0500

    update existing validation, started validation of 'fields'

--- validate.py
+++ validate.py
@@ -1,53 +1,69 @@
 import os
 import argparse
+import json
 
-def valid_readable_file(path):
-    # https://github.com/DCAN-Labs/CABINET/blob/main/src/utilities.py
-    """
-    Throw exception unless parameter is a valid readable filepath string. Use
-    this, not argparse.FileType("r") which leaves an open file handle.
-    :param path: Parameter to check if it represents a valid filepath
-    :return: String representing a valid filepath
-    """
-    return validate(path, lambda x: os.access(x, os.R_OK),
-                    os.path.abspath, "Cannot read file at '{}'")
+def directory(directory):
+    if not os.path.isdir(directory):
+        os.makedirs(directory)
+    return directory
 
-def validate(to_validate, is_real, make_valid, err_msg, prepare=None):
-    # https://github.com/DCAN-Labs/CABINET/blob/main/src/utilities.py
-    """
-    Parent/base function used by different type validation functions. Raises an
-    argparse.ArgumentTypeError if the input object is somehow invalid.
-    :param to_validate: String to check if it represents a valid object 
-    :param is_real: Function which returns true iff to_validate is real
-    :param make_valid: Function which returns a fully validated object
-    :param err_msg: String to show to user to tell them what is invalid
-    :param prepare: Function to run before validation
-    :return: to_validate, but fully validated
-    """
-    try:
-        if prepare:
-            prepare(to_validate)
-        assert is_real(to_validate)
-        return make_valid(to_validate)
-    except (OSError, TypeError, AssertionError, ValueError,
-            argparse.ArgumentTypeError):
-        raise argparse.ArgumentTypeError(err_msg.format(to_validate))
-
-class Directory:
-    def __init__(self, directory):
-        if not os.path.isdir(directory):
-            os.makedirs(directory)
-        self.directory = directory
+def data_source(source):
+    valid_sources = ["redcap", "qualtrics"]
+    if source in valid_sources:
+        return source
+    else:
+        raise argparse.ArgumentTypeError("\n{} is not a valid datasource. Please choose from {}".format(source, valid_sources))
     
-    def __str__(self) -> str:
-        return self.directory
+def instrument_json(file):
+    expected_keys = ["instrument_name", "instrument_name_loris", "metadata_fields", "validity_enabled", "validity_required", "pages", "fields", "groups"]
+    try:
+        assert os.access(file, os.R_OK)
+        path = os.path.abspath(file)
 
-class DataSource:
-    def __init__(self, source):
-        valid_sources = ["redcap", "qualtrics"]
-        if source in valid_sources:
-            self.source = source
-        else:
-            raise argparse.ArgumentTypeError("{} is not a valid datasource. Please choose from {}".format(source, valid_sources))
-    def __str__(self) -> str:
-        return self.source
+        with open(path) as json_file:
+            json_dict = json.load(json_file)
+            
+        for key in expected_keys:
+            if not key in json_dict:
+                raise argparse.ArgumentTypeError(f"\nInvalid instrument JSON:\nmissing key: {key}")
+
+        for field in json_dict["fields"]:
+            validate_field(field, json_dict["fields"][field])
+
+        return path
+    except (AssertionError):
+        raise argparse.ArgumentTypeError(f"\nCannot read file at {file}")
+    
+def validate_field(field_name, field_values):
+    required_keys = [
+        "field_name_loris",
+        "field_front_text_php",
+        "field_type_loris"
+    ]
+
+    # field_defaults = {
+    #     "field_include_not_answered": False,
+    #     "field_default_value": False,
+    #     "associated_status_field": False,
+    #     "page_php": 0,
+    #     "hidden_on_php": False,
+    #     "group_php": False,
+    #     "rule_php": False,
+    #     "note_php": False
+    # }
+
+    for key in required_keys:
+        if not key in field_values:
+            raise argparse.ArgumentTypeError(f"\nInvalid field '{field_name}' in instrument JSON:\nmissing key: {key}")
+
+    # for field in field_defaults.keys():
+    #     if field not in field_values:
+    #         field_values[field] = field_defaults[field]
+
+    if field_values["field_type_loris"] == "enum":
+        if not "enum_values_loris" in field_values.keys():
+            raise argparse.ArgumentTypeError(f"\nInvalid field '{field_name}' in instrument JSON\nmissing key 'enum_values_loris'")
+        if not "enum_values_php" in field_values.keys():
+            raise argparse.ArgumentTypeError(f"\nInvalid field '{field_name}' in instrument JSON:\nmissing key 'enum_values_php'")
+        if len(field_values["enum_values_loris"]) != len(field_values["enum_values_php"]):
+            raise argparse.ArgumentTypeError(f"\nInvalid Field '{field_name}' in instrument JSON:\n'enum_values_php' must be same length as 'enum_values_loris'")
