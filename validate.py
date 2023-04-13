4762644cacea906bf82335d6485f0e69002c3ea6
Barry Tikalsky <tikal004@umn.edu>
2023-04-13 12:46:12 -0500

    closes #9: updated _loris to _sql

--- validate.py
+++ validate.py
@@ -15,7 +15,7 @@ def data_source(source):
         raise argparse.ArgumentTypeError("\n{} is not a valid datasource. Please choose from {}".format(source, valid_sources))
     
 def instrument_json(file):
-    expected_keys = ["instrument_name", "instrument_name_loris", "metadata_fields", "validity_enabled", "validity_required", "pages", "fields", "groups"]
+    expected_keys = ["instrument_name", "instrument_name_sql", "metadata_fields", "validity_enabled", "validity_required", "pages", "fields", "groups"]
     try:
         assert os.access(file, os.R_OK)
         path = os.path.abspath(file)
@@ -40,8 +40,8 @@ def instrument_json(file):
 def validate_field(field_name, field_values, pages):
     # checks for requried keys
     required_keys = [
-        "field_name_loris",
-        "field_type_loris",
+        "field_name_sql",
+        "field_type_sql",
         "page_php"
     ]
     for key in required_keys:
@@ -55,13 +55,13 @@ def validate_field(field_name, field_values, pages):
                 raise argparse.ArgumentTypeError(f"\nInvalid field '{field_name}' in instrument JSON:\nmissing key: 'field_front_text_php'")
 
     # if it's an enum field, it must list its values.
-    if field_values["field_type_loris"] == "enum":
-        if not "enum_values_loris" in field_values.keys():
-            raise argparse.ArgumentTypeError(f"\nInvalid field '{field_name}' in instrument JSON\nmissing key 'enum_values_loris'")
+    if field_values["field_type_sql"] == "enum":
+        if not "enum_values_sql" in field_values.keys():
+            raise argparse.ArgumentTypeError(f"\nInvalid field '{field_name}' in instrument JSON\nmissing key 'enum_values_sql'")
         # Reuses sql values as php values if php values not defined. otherwise they must be the same length.
         if "enum_values_php" in field_values.keys():
-            if len(field_values["enum_values_loris"]) != len(field_values["enum_values_php"]):
-                raise argparse.ArgumentTypeError(f"\nInvalid Field '{field_name}' in instrument JSON:\n'enum_values_php' must be same length as 'enum_values_loris'")
+            if len(field_values["enum_values_sql"]) != len(field_values["enum_values_php"]):
+                raise argparse.ArgumentTypeError(f"\nInvalid Field '{field_name}' in instrument JSON:\n'enum_values_php' must be same length as 'enum_values_sql'")
             
     if field_values["page_php"] not in pages:
         raise argparse.ArgumentTypeError(f"\nInvalid field '{field_name}' in instrument JSON\ninvalid page number: {field_values['page_php']}")
